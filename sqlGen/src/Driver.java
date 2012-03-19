7c81e9560eaaa65f9023bf38982dd5987b0a36c6
William Blankenship <william.jblankenship@gmail.com>
2012-03-19 18:51:01 -0500

    Fixed the issue with merging two dictionaries together without repitions. One program generates a "Master List" that is a merge of the two lists without repitions, the other program takes a single file and inserts it into the database.

--- sqlGen/src/Driver.java
+++ sqlGen/src/Driver.java
@@ -15,46 +15,25 @@ public class Driver {
 		String createTable = "SET SQL_MODE=\"NO_AUTO_VALUE_ON_ZERO\";\n\n" +
 							 "CREATE TABLE IF NOT EXISTS `dictionary` (\n" +
 							 "  `pid` int(11) NOT NULL AUTO_INCREMENT,\n" +
-							 "  `password` varchar(125) NOT NULL,\n" +
-							 "  PRIMARY KEY (`pid`)\n" +
+							 "  `password` varchar(125) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,\n" +
+							 "  PRIMARY KEY (`pid`),\n" +
+							 "  UNIQUE (`password`)\n" +
 							 ") ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;\n\n";
-		String insertValues = "INSERT INTO `dictionary` (`password`) VALUES\n";
 		try {
-			PrintWriter sqlList = new PrintWriter("../buildDatabase");
-			sqlList.println(createDatabase + createTable + insertValues);
-			try {
-				Scanner passwordList = new Scanner(new File("../johnTheRipper.txt"));
-				while(passwordList.hasNextLine())
-				{
-					String line = passwordList.nextLine();
-					if(!line.contains("'"))
-						sqlList.println("('"+line+"'),");
-				}
-				passwordList.close();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			PrintWriter sqlDatabase = new PrintWriter(new File("../buildDatabase"));
+			sqlDatabase.println(createDatabase + createTable + "INSERT INTO `dictionary` (`password`) VALUES");
+			Scanner passwordList = new Scanner(new File("../masterDictionary.txt"));
+			
+			while(passwordList.hasNextLine())
+			{
+				String entry = passwordList.nextLine();
+				sqlDatabase.println("('"+entry+"'),");
 			}
-			try {
-				Scanner passwordList = new Scanner(new File("../cainAndAble.txt"));
-				while(passwordList.hasNextLine())
-				{
-					String line = passwordList.nextLine();
-					if(!line.contains("'"))
-						sqlList.println("('"+line+"'),");
-				}
-				passwordList.close();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			sqlList.println("('repen833');");
-			sqlList.close();
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			
+			sqlDatabase.println("('repen833');");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-
 	}
 
 }
