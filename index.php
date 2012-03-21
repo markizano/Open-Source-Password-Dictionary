c701b1ba9415798746f1ddf290f5a5b61d5cba16
Daniel Bugl <Daniel@touchlay.com>
2012-03-21 01:41:49 +0100

    Implemented the db folder, refined code, added link to the password list.

--- index.php
+++ index.php
@@ -6,25 +6,18 @@
 		<title></title>
 	</head>
 	<body>
-		<h2>ADD A PASSWORD TO THE LIST: </h2>
-		<form action="index.php" method="get">
-			<input type="text" name="password" />
-			<input type="submit" value="Add" />
-		</form>
-		<br />
-		<br />
-		<h2>Current List: </h2>
 		<?php
 			$passwordList = "default"; // Name of the password list
-			// $passwordList = "db/".$passwordList // Using a db folder
-			// $passwordList = $passwordList.".db" // Using a .db extension
+			mkdir("db"); if (file_exists("db")) $passwordList = "db/".$passwordList; // Using a db folder
+			$passwordList = $passwordList.".db"; // Using a .db extension
 			
 			// Listing all passwords
 			if ($_GET['action'] == "list") {
 				foreach (file($passwordList) as $linenum => $line) {
-					if ($_GET['mode'] == "linenum") print("#<b>".$linenum.": ");
+					if ($_GET['mode'] == "linenum") print("#<b>".$linenum.":</b> ");
 					print(htmlspecialchars($line));
-					print("<br />\n");
+					if ($_GET['mode'] != "raw") print("<br />");
+					print("\n");
 				}
 				exit();
 			}
@@ -34,14 +27,23 @@
 			
 			// TODO: Use AJAX to add passwords on return-key-press and without reload on button press
 			
+			// Adding the password
 			$newPassword = trim($_GET['password']); // Removing \n, \r and other stuff
-
-			if (isset($newPassword)) {
-				fwrite($passwordFile, $newPassword);
-			}
-			
+			fwrite($passwordFile, $newPassword);
 			fclose($passwordFile);
+			
+			print("Successfully added '".$newPassword."' to the list!");
 		?>
+		<p>
+			<h2>ADD A PASSWORD TO THE LIST: </h2>
+			<form action="index.php" method="get">
+				<input type="text" name="password" />
+				<input type="submit" value="Add" />
+			</form>
+		</p>
+		<p>
+			<a href="?action=list">Password list (<?php print($passwordList); ?>)</a>
+		</p>
 	</body>
 </html>
 
